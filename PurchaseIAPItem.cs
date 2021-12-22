7de9042a816d63f0932d3b7b20706d47ab708087
Danyal Hassan <64824576+DaniyalHassan046@users.noreply.github.com>
2021-12-22 21:59:56 +0500

    Add files via upload

---
+++ PurchaseIAPItem.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using SIS;
+using System;
+
+public class PurchaseIAPItem : MonoBehaviour
+{
+    public string productId;
+
+    [SerializeField]
+    Image imgIcon;
+    [SerializeField]
+    Text txtPrice;
+
+    Button btn;
+    IAPObject obj;
+
+    public UnityEvent AlreadyPurchased, OnPurchaseSuccussful;
+
+    void Start()
+    {
+        obj = IAPManager.GetIAPObject(productId);
+        if (imgIcon && obj.icon)
+        {
+            imgIcon.sprite = obj.icon;
+        }
+        if (txtPrice)
+        {
+            txtPrice.text = obj.realPrice;
+        }
+
+        btn = GetComponent<Button>();
+
+        if (btn)
+        {
+            btn.onClick.AddListener(BuyProduct);
+        }
+
+        if (DBManager.isPurchased(productId))
+        {
+            AlreadyPurchased.Invoke();
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnEnable()
+    {
+        IAPManager.purchaseSucceededEvent += PurchaseSuccessful;
+    }
+
+    void OnDisable()
+    {
+        IAPManager.purchaseSucceededEvent -= PurchaseSuccessful;
+    }
+
+    //make buy money function
+    public void BuyProduct()
+    {
+        IAPManager.PurchaseProduct(productId);
+    }
+
+    private void PurchaseSuccessful(string productId)
+    {
+        Debug.Log(productId);
+        Debug.Log(this.productId);
+
+        if (productId.Contains(this.productId))
+        {
+            OnPurchaseSuccussful.Invoke();
+        }
+    }
+
+}
---
+++ PurchaseIAPItems.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SIS;
+
+[System.Serializable]
+public class ItemToPuchase
+{
+    public string Item_id;
+    public GameObject[] Ad_btns;
+    public GameObject Offer;
+}
+
+public class PurchaseIAPItems : MonoBehaviour
+{
+    public ItemToPuchase[] Products;
+    public bool showOfferOnStart;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < Products.Length; i++)
+        {
+            if (DBManager.isPurchased(Products[i].Item_id))
+            {
+                ManageBtns(i, false);
+            }
+            else
+            {
+                ManageBtns(i, true);
+            }
+        }
+
+        if (showOfferOnStart)
+        {
+            for (int i = 0; i < Products.Length; i++)
+            {
+                if (!DBManager.isPurchased(Products[i].Item_id))
+                {
+                    ShowRandomOffer();
+                    break;
+                }
+            }
+        }
+    }
+
+
+    void OnEnable()
+    {
+
+        IAPManager.purchaseSucceededEvent += PurchaseSuccessful;
+        IAPManager.purchaseFailedEvent += PurchaseFailed;
+
+    }
+
+    void OnDisable()
+    {
+
+        IAPManager.purchaseSucceededEvent -= PurchaseSuccessful;
+        IAPManager.purchaseFailedEvent -= PurchaseFailed;
+    }
+
+    private void PurchaseSuccessful(string error)
+    {
+        for (int i = 0; i < Products.Length; i++)
+        {
+            if (DBManager.isPurchased(Products[i].Item_id))
+            {
+                ManageBtns(i, false);
+            }
+            else
+            {
+                ManageBtns(i, true);
+            }
+        }
+
+        if (Products[c_offer].Offer && Products[c_offer].Offer.activeSelf)
+        {
+            Products[c_offer].Offer.SetActive(false);
+        }
+    }
+    private void PurchaseFailed(string error)
+    {
+
+    }
+    public void PuchaseNow(int Val)
+    {
+        IAPManager.PurchaseProduct(Products[Val].Item_id);
+    }
+
+    public void ManageBtns(int No, bool Active)
+    {
+        for (int j = 0; j < Products[No].Ad_btns.Length; j++)
+        {
+            if (Products[No].Ad_btns[j] && (Products[No].Ad_btns[j].activeSelf != Active))
+                Products[No].Ad_btns[j].SetActive(Active);
+        }
+    }
+
+    int count = 0;
+    int c_offer = 0;
+    public void ShowRandomOffer()
+    {
+        c_offer = Random.Range(0, Products.Length);
+
+        if (c_offer == PlayerPrefs.GetInt("LastOffer", -1) || DBManager.isPurchased(Products[c_offer].Item_id))
+        {
+            if (count >= 5)
+            {
+                return;
+            }
+            count++;
+            ShowRandomOffer();
+            return;
+        }
+
+        if (Products[c_offer].Offer)
+        {
+            PlayerPrefs.SetInt("LastOffer", c_offer);
+            Products[c_offer].Offer.SetActive(true);
+        }
+    }
+    public void RemoveAds()
+    {
+        
+    }
+    public void Everything()
+    {
+        IAPManager.PurchaseProduct("everything");
+        if (DBManager.isPurchased("everything"))
+        {
+            DBManager.SetPurchase("unlock_all_vehicles", 1);
+            DBManager.SetPurchase("no_ads", 1);
+            DBManager.SetPurchase("unlock_all_levels", 1);
+            DBManager.SetPurchase("unlock_all_accessories", 1);
+        }
+    }
+}
